4f8e9053bbfef2148cf1d4105ab6d8436f7253b6
Gabriel B <gabriel.moura2009@hotmail.com>
2020-10-23 23:55:39 -0300

    third day finished

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanageMap from './pages/OrphanageMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
     return (
@@ -10,6 +12,10 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing}/>
                 <Route path="/app" component={OrphanageMap}/>
+
+                <Route path="/orphanage/create" component={CreateOrphanage}/>
+                <Route path="/orphanage/:id" component={Orphanage}/>
+
             </Switch>
         </BrowserRouter>
     );
